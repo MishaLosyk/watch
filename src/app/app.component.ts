abe2806339015f74e7c8424da3ee8fa7924a93c2
MishaLosyk <44748229+MishaLosyk@users.noreply.github.com>
2020-07-22 17:55:49 +0300

    changed router navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,24 +7,12 @@ import {ActivatedRoute, Router, UrlSegment} from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'films';
-  line: boolean;
-  line2: boolean;
-  line3: boolean;
+
 
   constructor (private route: ActivatedRoute) {
 
-    if(window.location.href.search('home') != -1) {this.line = true};
-    if(window.location.href.search('about') != -1) {this.line2 = true};
-    if(window.location.href.search('contacts') != -1) {this.line3 = true};
 
   }
-
- resetLine() {
-   this.line = false;
-   this.line2 = false;
-   this.line3 = false;
- }
 
 
 }
